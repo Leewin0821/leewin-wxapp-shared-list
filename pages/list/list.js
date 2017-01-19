0e7c9a122f5313299bf04aa31d0b50b7be68a827
lwzhang <lwzhang@thoughtworks.com>
2017-01-19 15:31:55 +0800

    [#1] Cleaned code in list.js

--- pages/list/list.js
+++ pages/list/list.js
@@ -4,53 +4,15 @@ Page({
     data: {
         userInfo: {},
         items: [
-            {name: 'AR/VR Workshop', value: 'AR', checked: false, showBtn: false},
-            {name: 'Mini Program Workshop', value: 'Mini', checked: true, showBtn: false},
-            {name: 'Ng2 Practice', value: 'Ng2', checked: false, showBtn: false},
-            {name: 'React Native Practice', value: 'RN', checked: false, showBtn: false},
-            {name: 'Scalling Lean', value: 'Lean', checked: false, showBtn: false},
-            {name: 'Holiday', value: 'Hol', checked: true, showBtn: false}
+            {name: 'AR/VR Workshop', checked: false},
+            {name: 'Mini Program Workshop', checked: true},
+            {name: 'Ng2 Practice', checked: false},
+            {name: 'React Native Practice', checked: false},
+            {name: 'Scalling Lean', checked: false},
+            {name: 'Holiday', checked: true}
         ]
     },
-    add(event) {
-        var that = this;
-        that.data.items.unshift({
-            name: event.detail.value,
-            value: 'add',
-            checked: false,
-            showBtn: false
-        })
-        this.setData({
-            items: that.data.items
-        })
-        return ''
-    },
-    bindCheck(event) {
-        var that = this;
-        var index = event.currentTarget.dataset.checkedboxindex;
-        that.data.items[index].checked = !that.data.items[index].checked;
-        this.setData({
-            items: that.data.items
-        })
-    },
-    bindLongTap(event) {
-        var that = this;
-        var index = event.currentTarget.dataset.checkedboxindex;
-        that.data.items[index].showBtn = !that.data.items[index].showBtn;
-        this.setData({
-            items: that.data.items
-        })
-    },
-    remove(event) {
-        var that = this;
-        var index = event.currentTarget.dataset.checkedboxindex;
-        that.data.items.splice(index, 1)
-        this.setData({
-            items: that.data.items
-        })
-    },
     onLoad() {
-        console.log('onLoad')
         var that = this
         //调用应用实例的方法获取全局数据
         app.getUserInfo(function (userInfo) {
@@ -60,11 +22,13 @@ Page({
             })
         })
     },
-    onShareAppMessage() {
-        return {
-            title: '自定义分享标题',
-            desc: '自定义分享描述',
-            path: '/page/user?id=123'
+    checkboxChange(event) {
+        let items = this.data.items
+        for (let item of items) {
+            item.checked = event.detail.value.indexOf(String(items.indexOf(item))) !== -1;
         }
+        this.setData({
+            items: items
+        })
     }
 })
